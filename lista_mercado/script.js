1acee0783324d2f6242c33de1f89f2040d9ce25a
Taissa <issa.gelinski@gmail.com>
2023-03-16 20:00:06 -0300

    Exercicio

---
+++ lista_mercado/script.js
@@ -0,0 +1,33 @@
+const input = window.document.getElementById("input");
+const ul = window.document.querySelector("ul");
+function adicionar() {
+  var li = window.document.createElement("li");
+  var value = input.value;
+  var value_li = window.document.createTextNode(value);
+
+  if (input.value.trim() !== ""){
+  li.appendChild(value_li);
+  ul.appendChild(li);
+  }
+
+  input.value="";
+  input.focus();
+}
+
+window.document.querySelector("ul").addEventListener("click", (e)=> {
+    e.target.classList.toggle("checked");
+});
+
+window.document.getElementById("input").addEventListener("keypress", (e)=> {
+ if (e.key == "Enter") {
+    adicionar();
+ }
+ });
+ 
+ window.document.addEventListener("contextmenu" , (e) => {
+    e.preventDefault();
+    if(e.target.tagName === "LI"){
+    e.target.remove();
+    }
+    
+ })
