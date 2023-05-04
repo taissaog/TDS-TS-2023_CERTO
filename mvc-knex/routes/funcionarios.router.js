2db0ffa0dc474a517d59d3c8430db0d2f003dd7b
taissaog <110946885+taissaog@users.noreply.github.com>
2023-05-03 22:16:11 -0300

    Delete mvc-knex directory

--- mvc-knex/routes/funcionarios.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const funcionariosController = require("../controller/funcionarios.controller");
-
-const routes = new express.Router();
-
-routes.get("/", funcionariosController.buscaTodos);
-routes.get("/:id[0-9]", funcionariosController.buscaTodos);
-
-module.exports = routes;
