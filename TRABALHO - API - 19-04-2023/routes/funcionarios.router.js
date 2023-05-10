51b2a60fa5b032e514ff8ccddbb9a54a52e03c54
taissaog <110946885+taissaog@users.noreply.github.com>
2023-05-09 21:53:07 -0300

    Delete TRABALHO - API - 19-04-2023 directory

--- TRABALHO - API - 19-04-2023/routes/funcionarios.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"); //para usar no pacote
-
-const routes = new express.Router(); //rotas recebe nova rota
-
-routes.get("/"); //procura dentro do funcionarios / alguma coisa
-
-module.exports = routes; //para poder importar/export
-
